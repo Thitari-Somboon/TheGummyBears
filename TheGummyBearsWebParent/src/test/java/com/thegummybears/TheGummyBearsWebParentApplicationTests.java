8f239be28361140c8732bcdc05f902e6eb99326b
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-03-12 19:05:48 +0100

    first commit

---
+++ TheGummyBearsWebParent/src/test/java/com/thegummybears/TheGummyBearsWebParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegummybears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGummyBearsWebParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
