caf23946a24396282812b214d88dac904189bd33
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-03-16 14:45:35 +0100

    commit files

--- TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/user/RoleRepository.java
+++ TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/user/RoleRepository.java
@@ -3,6 +3,8 @@ package com.thegummybears.admin.user;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.thegummybears.common.entity.Role;
+
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Integer>{
 
