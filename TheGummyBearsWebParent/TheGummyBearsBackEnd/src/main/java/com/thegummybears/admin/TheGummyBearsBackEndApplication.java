2a7975f56baf8d1578a5083fc062e93daddd9f55
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-03-16 19:19:04 +0100

    database created in mySQL

--- TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/TheGummyBearsBackEndApplication.java
+++ TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/TheGummyBearsBackEndApplication.java
@@ -2,8 +2,10 @@ package com.thegummybears.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.thegummybears.common.entity", "com.thegummybears.admin.user"})
 public class TheGummyBearsBackEndApplication {
 
 	public static void main(String[] args) {
