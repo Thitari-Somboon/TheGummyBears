58c070687bedb0bc2af5b88b1748f3bf80da4821
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-07 20:11:39 +0200

    [28] User form created

--- TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/user/UserController.java
+++ TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/user/UserController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.thegummybears.common.entity.Role;
 import com.thegummybears.common.entity.User;
 
 @Controller
@@ -13,9 +17,36 @@ public class UserController {
 	
 	@Autowired
 	private UserService service;
+	
+	
+	@GetMapping("/users")
+	public String listAll(Model model) {
+		List<User> listUsers = service.listAll();
+		model.addAttribute("listUsers", listUsers);
 
-	//@GetMapping("/users")
-//	public String listAll(Model model) {
-//		List<User> listUsers = service.listAll()
-	//}
+		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+
+		User user = new User();
+		user.setEnabled(true);
+
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+
+		return "user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+
+		return "redirect:/users";
+	}
 }
