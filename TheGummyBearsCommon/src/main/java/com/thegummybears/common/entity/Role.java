6afa3d23b927a047678d67b9e20b664fa7105d29
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-07 17:23:43 +0200

    [26] Role.java create equals() and hashcode()

--- TheGummyBearsCommon/src/main/java/com/thegummybears/common/entity/Role.java
+++ TheGummyBearsCommon/src/main/java/com/thegummybears/common/entity/Role.java
@@ -24,9 +24,9 @@ public class Role {
 	public Role() {
 	}
 	
-//	public Role(Integer id) {
-//		this.id = id;
-//	}
+	public Role(Integer id) {
+		this.id = id;
+	}
 	
 	public Role(String name) {
 		this.name = name;
@@ -36,6 +36,7 @@ public class Role {
 		this.name = name;
 		this.description = description;
 	}
+
 
 	public Integer getId() {
 		return id;
@@ -65,4 +66,31 @@ public class Role {
 	public String toString() {
 		return this.name;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
