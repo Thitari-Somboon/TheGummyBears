9ddbd6987abf4e4fd3835469803869a2ac96e0a3
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-07 16:57:51 +0200

    [26] Run testing with empty method to create two tables

--- TheGummyBearsWebParent/TheGummyBearsBackEnd/src/test/java/com/thegummybears/admin/user/UserRepositoryTest.java
+++ TheGummyBearsWebParent/TheGummyBearsBackEnd/src/test/java/com/thegummybears/admin/user/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.thegummybears.admin.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,5 +11,19 @@ import org.springframework.test.annotation.Rollback;
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 public class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository repo;
+	
+	
+	//Run Junit with empty test to create the table in DB
+	//It will create 
+	//1 users table and 
+	//2 users_roles table
+	@Test
+	public void testCreateUser() {
+		
+	}
+
 
 }
