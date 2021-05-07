31e97cb5f7989ce5f432c5ec62ce960874ab9c15
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-07 17:32:42 +0200

    [26] UserRepositoryTest, CreateNewUser with two roles success

--- TheGummyBearsWebParent/TheGummyBearsBackEnd/src/test/java/com/thegummybears/admin/user/UserRepositoryTest.java
+++ TheGummyBearsWebParent/TheGummyBearsBackEnd/src/test/java/com/thegummybears/admin/user/UserRepositoryTest.java
@@ -30,8 +30,19 @@ public class UserRepositoryTest {
 	//2 users_roles table
 	//-------------------
 	//Update method
+	/*
+	 * @Test public void testCreateUser() { Role roleAdmin =
+	 * enitiyManager.find(Role.class, 1); //Assign to be Admin User userThitari =
+	 * new User("Thitari@gmail.com", "password123", "Thitari", "Somboon", 110.0);
+	 * userThitari.addRole(roleAdmin);
+	 * 
+	 * User savedUser = repo.save(userThitari);
+	 * assertThat(savedUser.getId()).isGreaterThan(0); //Object save as persistant
+	 * object }
+	 * 
+	 */	
 	@Test
-	public void testCreateUser() {
+	public void testCreateNewUserwithOneRole() {
 		Role roleAdmin = enitiyManager.find(Role.class, 1); //Assign to be Admin 
 		User userThitari = new User("Thitari@gmail.com", "password123", "Thitari", "Somboon", 110.0);	
 		userThitari.addRole(roleAdmin);
@@ -39,6 +50,17 @@ public class UserRepositoryTest {
 		User savedUser =  repo.save(userThitari);
 		assertThat(savedUser.getId()).isGreaterThan(0); //Object save as persistant object
 	}
-
+	
+	@Test
+	public void testCreateNewUserwithTwoRoles() {
+		User userOla = new User("Ola@gmail.com", "password000", "Olga", "Osipova", 250.00);	
+		Role roleAdmin = new Role(1);
+		Role roleEmployee = new Role(2);
+		userOla.addRole(roleAdmin);
+		userOla.addRole(roleEmployee);
+		
+		User savedUser =  repo.save(userOla);
+		assertThat(savedUser.getId()).isGreaterThan(0); //Object save as persistant object
+	}
 
 }
