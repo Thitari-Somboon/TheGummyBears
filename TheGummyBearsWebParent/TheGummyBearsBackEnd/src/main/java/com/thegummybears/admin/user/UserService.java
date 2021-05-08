7cfae54872d74e72579ab347303d07a9f45ec27b
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-08 14:08:30 +0200

    [32] Code update user function

--- TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/user/UserService.java
+++ TheGummyBearsWebParent/TheGummyBearsBackEnd/src/main/java/com/thegummybears/admin/user/UserService.java
@@ -1,6 +1,7 @@
 package com.thegummybears.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,7 +31,20 @@ public class UserService {
 	}
 
 	public void save(User user) {
+		boolean isUpdatingUser = (user.getId() != null);
+		
+		if(isUpdatingUser) {
+			User existingUser =  userRepo.findById(user.getId()).get();
+			
+			if(user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			}else {
+				encoderPassword(user);
+			}
+		}else {
 		encoderPassword(user);
+		}
+		
 		userRepo.save(user);
 	}
 	
@@ -39,10 +53,28 @@ public class UserService {
 		user.setPassword(encodedPassword);
 	}
 	
-	public boolean isEmailUnique(String email) {
+	public boolean isEmailUnique(Integer id, String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
-		return userByEmail == null;
+		if(userByEmail == null) return true;
+		
+		boolean isCreatingNew = (id == null);
+		if(isCreatingNew) {
+			if(userByEmail != null) return false;
+		}else {
+			//Email is not unique
+			if(userByEmail.getId() != id) {
+				return false;
+			}
+		}
+		return true;
 	}
 
+	public User get(Integer id) throws UserNotFoundException {
+		try {
+		return userRepo.findById(id).get();
+		}catch (NoSuchElementException e) {
+			throw new UserNotFoundException("Could not find any user with ID " + id);
+		}
+	}
 }
 
